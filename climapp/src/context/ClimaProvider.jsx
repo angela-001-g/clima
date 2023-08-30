2ac71d1fe340b7dc52f15a30f164b95436cd4e7f
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-08-30 16:20:39 -0500

    consultando el clima en la API

--- climapp/src/context/ClimaProvider.jsx
+++ climapp/src/context/ClimaProvider.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState, createContext } from 'react'
+import axios from 'axios'
 
 const ClimaContext = createContext();
 
@@ -12,6 +13,8 @@ const ClimaProvider = ({children}) => {
         pais: ''
     })
 
+    const [resultado, setResultado] = useState({})
+
     const datosBusqueda = e => {
         setBusqueda({
             ...busqueda,
@@ -19,8 +22,22 @@ const ClimaProvider = ({children}) => {
         })
     }
 
-    const consultarClima = datos => {
-        console.log(datos)
+    const consultarClima = async datos => {
+        try{
+            const {ciudad, pais} = datos
+            const appId = import.meta.env.VITE_API_KEY
+            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`
+        
+            const { data } = await axios(url)
+            const { lat, lon } = data[0]
+
+            const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+            const { data: clima } = await axios(urlClima)
+            setResultado(clima)
+
+        } catch(error){
+            console.log(error)
+        }
     }
 
     return(
@@ -28,7 +45,8 @@ const ClimaProvider = ({children}) => {
             value={{ 
                 busqueda, 
                 datosBusqueda,
-                consultarClima
+                consultarClima, 
+                resultado
             }}
         >
             {children}
